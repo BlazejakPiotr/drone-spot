8850fc0e9d8fb62820e6b13ea26e97bd06eff364
Piotr Błażejak <piotr.blazejak@yahoo.com>
2022-05-27 23:56:54 +0200

    home

--- App.js
+++ App.js
@@ -1,20 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+
+import { useFonts, Roboto_400Regular } from "@expo-google-fonts/roboto";
+import AppLoading from "expo-app-loading";
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+  });
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.wrapper}>
+        <View>
+          <Text>Drone Spot</Text>
+        </View>
+        <View>
+          <TouchableOpacity style={styles.button}>
+            <Text>Log in</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.button}>Sign In</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    marginVertical: 60,
+    backgroundColor: "#f3f3f3",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "column",
+    justifyContent: "space-between",
+    color: "#fff",
+  },
+  wrapper: {
+    paddingHorizontal: 14,
+  },
+  button: {
+    marginBottom: 12,
+    height: 50,
+    backgroundColor: "#386596",
+    borderRadius: 5,
+    color: "#fff",
+    alignItems: "center",
+    fontFamily: Roboto_400Regular,
   },
 });
